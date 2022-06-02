a66d2ff706ad469a89c32ebe7f28f66b8fff6d95
setback-king <90795553+setback-king@users.noreply.github.com>
2022-06-02 10:53:43 -0400

    initial commit

---
+++ files.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+/* fs.mkdir("./files", (err) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log("directory created");
+});
+ */
+
+fs.writeFile("./index.js", "", () => {
+  console.log("file written");
+});
---
+++ index.js
@@ -0,0 +1,37 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  let path = "./files/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/contact-me":
+      path += "contact-me.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
+});
+
+server.listen(8080, "localhost", () => {
+  console.log("listening for requests on port 8080");
+});
